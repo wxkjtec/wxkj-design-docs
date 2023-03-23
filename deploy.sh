28ffc61d404dffeeadd360f3fecb18592d6d0efa
Dm-China <13510830600@163.com>
2023-03-23 23:03:22 +0800

    :wrench:fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 git init
-git remote add origin git@github.com:Dm-China/Dm-China.github.io.git
+git remote add origin git@github.com:wxkjtec/wxkj-design-docs.git
 git add -A
 git commit -m 'deploy page'
 
 
 # git pull
 git push -f origin main
-# git push -f git@github.com:Dm-China/Dm-China.github.io.git master
+# git push -f git@github.com:wxkjtec/wxkj-design-docs.git
 
 cd -
